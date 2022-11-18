e8e99b67ec469e21ccd0d9286d69277af5e71fc5
kaisujay <kai.sujay@gmail.com>
2022-11-18 21:01:52 +0530

    Removed "Style Binding"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  color:string='green';
 
-  changeColor()
-  {
-    this.color = 'pink';
-  }
 }
