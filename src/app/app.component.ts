22f250cda6bfcacf54de016ca6c103f5b752e631
kaisujay <kai.sujay@gmail.com>
2022-11-18 15:18:19 +0530

    Removed "Interpolation" and "Strict"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  Name: string = 'Sujay Majumder'; //No Private. ByDefault it is Public
-  //Name = 'Sujay Majumder'; //If not mentioning ":string" that means it is ":any"
-
-  getName(): string { //":string" is the return type
-    return "Hello - " + this.Name;
-  }
-
-  getNameWithAge(data: number): any { //":string" is the return type
-    return "Hello - " + this.Name + " Age - " + data;
-  }
-
-  Names = ["jay", "mon", "raj"];  //Array
-
-  User = {  //Object
-    FullName :'Sujay Majumder',
-    Age : 25
-  }
+ 
 
 }
