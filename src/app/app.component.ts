8c95065c5fe0630f21ec51dc08ccf70e06441ba1
kaisujay <kai.sujay@gmail.com>
2022-11-18 15:41:41 +0530

    Get Input Box Value and Display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
 
+  WithOutButtonClick:string='';
+  WithButtonClick:string='';
+
+  getName(value:string)
+  {
+    console.log(value);
+    this.WithOutButtonClick=value;
+  }
+
+
+  getName1(value:string)
+  {
+    console.log(value);
+    this.WithButtonClick=value;
+  }
 }
