e662ce9c425a71a6de584cfa9fcf62ac995665cc
kaisujay <kai.sujay@gmail.com>
2022-11-18 19:56:52 +0530

    Removed "Nested For Loop"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  Users=[
-    {
-      Name:'Jay',
-      Age:24,
-      Social:['FB','WA']
-    },
-    {
-      Name:'Nikki',
-      Age:21,
-      Social:['Insta','FB','WA']
-    },
-    {
-      Name:'Sunny',
-      Age:22,
-      Social:['SnapChat','Twitter']
-    }
-  ]
+  
 }
