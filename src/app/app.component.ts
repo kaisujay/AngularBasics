059766409ff5cba7cafaa3fe5afde8e8f82df175
kaisujay <kai.sujay@gmail.com>
2022-11-18 15:57:00 +0530

    Added Counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  clicked: number = 0;
 
+  calculateClicked(value: string) {
+    if (value === 'add') {
+      this.clicked++;
+    }
+    else {
+      this.clicked --;
+    }
+  }
 }
