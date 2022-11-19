584b49c84771803073464a8ea0f9fd2fdf0fb5c3
kaisujay <kai.sujay@gmail.com>
2022-11-19 21:54:04 +0530

    Reactive Form Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Data } from '@angular/router';
-import { FormControl,FormGroup } from '@angular/forms';
+import { FormControl,FormGroup,Validators } from '@angular/forms';
 
 
 @Component({
@@ -11,12 +11,22 @@ import { FormControl,FormGroup } from '@angular/forms';
 
 export class AppComponent {
   userForm=new FormGroup({
-    txtBoxName:new FormControl(''),
-    txtBoxAge:new FormControl('')
+    txtBoxName:new FormControl('',[Validators.required]),
+    txtBoxEmail:new FormControl('',[Validators.required,Validators.email])
   });
     
   getFormValue()
   {
     console.warn(this.userForm.value);
+  }
+
+  get nameValidation()
+  {
+    return this.userForm.get('txtBoxName');
+  }
+
+  get emailValidation()
+  {
+    return this.userForm.get('txtBoxEmail');
   }
 }
