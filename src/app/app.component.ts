f6eb471e2e1136b2b2bfb2324416f1c02b58b037
kaisujay <kai.sujay@gmail.com>
2022-11-19 13:59:21 +0530

    Removed "Project : ToDo List"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  toDoList: string[] = []; //Empty String Array
-
-  addItem(value: string) {
-    this.toDoList.push(value);
-  }
-
-  removeItem(value: string) {
-    const index = this.toDoList.indexOf(value, 0); 
-    this.toDoList.splice(index, 1); //To Remove we have to use "splice"
-
-    this.toDoList = this.toDoList.filter(x=>x !== value); // Another way to remove from arary
-  }
 }
