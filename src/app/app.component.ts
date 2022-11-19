006d4f4dff1ba4e8f12c56b57ec7b690a35ba12e
kaisujay <kai.sujay@gmail.com>
2022-11-19 16:42:30 +0530

    Removed "Create Component for "Pass Data Child to Parent""

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  result:any='';
-  
-  updateDataForParent(data:Event)
-  {
-    //console.warn(data);
-    this.result=data;
-  }
+    
 }
