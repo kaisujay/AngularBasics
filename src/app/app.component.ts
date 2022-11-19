8f45a2720b456e8283cafd11ca8105c626db8c99
kaisujay <kai.sujay@gmail.com>
2022-11-19 21:55:21 +0530

    Remove "Reactive Form Validation"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Data } from '@angular/router';
-import { FormControl,FormGroup,Validators } from '@angular/forms';
 
 
 @Component({
@@ -10,23 +9,5 @@ import { FormControl,FormGroup,Validators } from '@angular/forms';
 })
 
 export class AppComponent {
-  userForm=new FormGroup({
-    txtBoxName:new FormControl('',[Validators.required]),
-    txtBoxEmail:new FormControl('',[Validators.required,Validators.email])
-  });
-    
-  getFormValue()
-  {
-    console.warn(this.userForm.value);
-  }
-
-  get nameValidation()
-  {
-    return this.userForm.get('txtBoxName');
-  }
-
-  get emailValidation()
-  {
-    return this.userForm.get('txtBoxEmail');
-  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    ReactiveFormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
