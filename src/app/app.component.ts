bf79a8927d0f4c8045158f4343c0bfd751b8479b
kaisujay <kai.sujay@gmail.com>
2022-11-19 21:35:38 +0530

    Reactive Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Data } from '@angular/router';
+import { FormControl,FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,8 +9,14 @@ import { Data } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 
-
 export class AppComponent {
-
+  userForm=new FormGroup({
+    txtBoxName:new FormControl(''),
+    txtBoxAge:new FormControl('')
+  });
     
+  getFormValue()
+  {
+    console.warn(this.userForm.value);
+  }
 }
