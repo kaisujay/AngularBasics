c97d2c7100eb670a019f3090cc596e13d1a49ccb
kaisujay <kai.sujay@gmail.com>
2022-11-20 11:14:29 +0530

    Removed "Reouting"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
-import { ContactusComponent } from './contactus/contactus.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AboutusSalesTeamComponent } from './aboutus/aboutus-sales-team/aboutus-sales-team.component';
-import { AboutusCustomercareTeamComponent } from './aboutus/aboutus-customercare-team/aboutus-customercare-team.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutusComponent,
-    ContactusComponent,
-    PageNotFoundComponent,
-    AboutusSalesTeamComponent,
-    AboutusCustomercareTeamComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
