4684012e49fa40cfc1077837363472109fda3786
kaisujay <kai.sujay@gmail.com>
2022-11-20 14:23:04 +0530

    Removed "HTTP Services"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Data } from '@angular/router';
-import {UsersataService} from './services/usersata.service';
 
 @Component({
   selector: 'app-root',
@@ -9,30 +8,8 @@ import {UsersataService} from './services/usersata.service';
 })
 
 export class AppComponent {
- 
-  users:any;
 
-  constructor(private userdata: UsersataService) {
-    userdata.getUsers().subscribe((data)=>{
-      console.warn("data",data);
-      
-      this.users=data});
+  constructor() {
   }
 
-  dummyData=[  //some Dummy Data which will come from some kind of "Forms"
-    {
-      code: "INR",
-      symbol: "&#22;",
-      rate: "12,456.3456",
-      description: "Indian Rupees",
-      rate_float: 14346.6578
-    }
-  ]
-
-  saveDummyData(dummyData:any)    //just a Dummy Save
-  {
-    this.userdata.saveUsers(dummyData).subscribe((result)=>{
-      console.warn(result);      
-    });
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";    //We have to Add here also.
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
