388825ab70f49942130af4f084ed470240a11af8
kaisujay <kai.sujay@gmail.com>
2022-11-20 12:27:40 +0530

    Services "Created 2 new components for Services"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { User1Component } from './user1/user1.component';
+import { User2Component } from './user2/user2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    User1Component,
+    User2Component
   ],
   imports: [
     BrowserModule
